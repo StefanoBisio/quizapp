a36472ceaf941744bd95dbd34950e5553129079b
Stefano Bisio <s.b.milano@gmail.com>
2023-08-08 20:11:45 +0100

    add comments and adjust formatting

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import router from './router/index';
 
 import './assets/main.css'
 
+// Create a new Vue application using the App component as the root component.
 const app = createApp(App);
 
+// Use the router in your Vue application. This enables Vue Router's functionality in your app.
 app.use(router);
 
+// Mount the Vue application to the DOM. The '#app' selector is the id of the HTML element where your Vue app will be mounted.
 app.mount('#app');
