37c8fc1b62af74e5bc343ef2090eea0b8d40f673
Stefano Bisio <s.b.milano@gmail.com>
2023-08-09 22:32:50 +0100

    dynamically change the question status (0/3)

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -2,7 +2,7 @@
     import Question from "../components/Question.vue"
     import QuizHeader from "../components/QuizHeader.vue"
     import {useRoute} from "vue-router"
-    import { ref } from "vue";
+    import { ref, watch } from "vue";
 
     import quizes from "../data/quizes.json"
 
@@ -14,13 +14,26 @@
 
     const currentQuestionIndex = ref(0);
 
+    const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`);
+
+    watch(
+
+        //value to watch
+        () => currentQuestionIndex.value, 
+        
+        //function to run when that changes
+        () => questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
+    )
+
 </script>
 
 <template>
     <div>
-        <QuizHeader />
+        <QuizHeader :questionStatus="questionStatus" />
         <div>
             <Question :question="quiz.questions[currentQuestionIndex]"/>
         </div>
+        <!-- testing only, this button will be deleted -->
+        <button @click="currentQuestionIndex++">change question status</button>
     </div>
 </template>
